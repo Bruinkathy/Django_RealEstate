76adbc65fcc9c5eabc3b821208d57a39e46bc3a4
Kathy Bartholomew <bruinkathy@gmail.com>
2021-01-12 12:22:08 -0800

    Markup for Home, About pages, Linking, Active Links

--- djre/settings.py
+++ djre/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os
+from pathlib import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,4 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'djre/static')
+]
