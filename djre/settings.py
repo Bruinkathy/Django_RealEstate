676991cfa2d4369111035bc3cadf37eaca78b6ec
Kathy Bartholomew <bruinkathy@gmail.com>
2021-01-13 14:58:49 -0800

    Created Models, Migrations, Postgres DB Tables

--- djre/settings.py
+++ djre/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'djre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Secret',
+        'HOST': 'localhost'
     }
 }
 
