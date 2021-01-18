021015c6d6170d990618455df8ed5b0c3d0a93e1
Kathy Bartholomew <bruinkathy@gmail.com>
2021-01-18 12:21:38 -0800

    Admin Setup, Customization, Filters, etc.

--- djre/settings.py
+++ djre/settings.py
@@ -117,3 +117,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'djre/static')
 ]
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
