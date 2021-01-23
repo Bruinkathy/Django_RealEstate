85e156da346365999f5864b11d13e9e41821bb4e
Kathy Bartholomew <bruinkathy@gmail.com>
2021-01-22 16:28:52 -0800

    Retrieve Data from DB to Pages

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Realtor
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display = ('id', 'name', 'email', 'is_mvp', 'hire_date')
     list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
     search_fields = ('name',)
     list_per_page = 25
 
